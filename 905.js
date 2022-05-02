4e46f335cd9a8649131fe5826cb4d14da091f3cf
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-02 10:10:01 -0700

    Update 905.js

--- 905.js
+++ 905.js
@@ -17,3 +17,22 @@ const sortArrayByParity = function(nums) {
     }
     return [...arr, ...nums];
 };
+
+
+// Second submission, accepted
+
+const sortArrayByParity = function(nums) {
+    const arr = [];
+    for (let i = nums.length -1; i >= 0; i--) {
+        if (nums[i] % 2 == 0) {
+            arr.unshift(nums.pop(nums[i]));
+        } else {
+            arr.push(nums.pop(nums[i]));
+        }
+    }
+    
+    return [...arr];
+};
+
+//Runtime: 120 ms, faster than 29.26% of JavaScript online submissions for Sort Array By Parity.
+//Memory Usage: 44.8 MB, less than 42.95% of JavaScript online submissions for Sort Array By Parity.
