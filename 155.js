c24f6cfd248080d5958063ba47a1c21a59753740
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-04 17:31:48 -0700

    Update 155.js

--- 155.js
+++ 155.js
@@ -47,3 +47,22 @@ MinStack.prototype.getMin = function() {
         return this.min[this.min.length - 1];
     }
 };
+
+// Second submission, I recoded the last two functions, top() and getMin(), to use the ternary operator instead of if statements to see how it would affect the runtime and memory usage.
+// It turns out that it's slower, but uses up less memory
+//Runtime: 146 ms, faster than 55.19% of JavaScript online submissions for Min Stack. cf. 115 ms, faster than 79.29%
+//Memory Usage: 49.1 MB, less than 94.08% of JavaScript online submissions for Min Stack. cf. 49.8 MB, less than 62.30%
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack.length ? this.stack[this.stack.length -1] : null;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min.length ? this.min[this.min.length -1] : null;
+};
