1357d84174adb09818e8818781ad9e2394f3e162
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-05 10:12:37 -0700

    Update 003.js

--- 003.js
+++ 003.js
@@ -16,7 +16,40 @@ const lengthOfLongestSubstring = function(s) {
             current.splice(0, currentChar + 1);
         }
         current.push(s[i]);
-        longest = Math.max( longest, current.length);
+        longest = Math.max(longest, current.length);
     }
+    return longest;
+};
+
+// Found this other solution using Sets, saving for myself to study
+
+const lengthOfLongestSubstring = function (s) {
+    // Initialise a set to store the longest string in
+    let longest = 0;
+
+    // Loop through the provided string
+    for (let i = 0; i < s.length; i++) {
+        // Initialise a set to store the string created from the current point
+        let current = new Set();
+
+        // Loop through the letters from the current point
+        for (let x = i; x < s.length; x++) {
+            // Check if the current letter exists in the current Set
+            if (current.has(s[x])) {
+                // Move on from the current letter without adding it (as it already exists in the set)
+                break;
+            } else {
+                // Character not found, add it to the set
+                current.add(s[x]);
+            }
+        }
+
+        // Update the longest string length (if this one was bigger)
+        longest = Math.max(
+            longest,
+            current.size
+        );
+    }
+
     return longest;
 };
