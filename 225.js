da09ac2a786c09a7f415f3d1bd1518131103aae0
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-04 18:03:15 -0700

    Update 225.js

--- 225.js
+++ 225.js
@@ -32,3 +32,42 @@ MyStack.prototype.top = function() {
 MyStack.prototype.empty = function() {
     return this.queue ? false : true;
 };
+
+// Second Submission
+// Runtime: 57 ms, faster than 92.45% of JavaScript online submissions for Implement Stack using Queues.
+// Memory Usage: 42 MB, less than 49.89% of JavaScript online submissions for Implement Stack using Queues.
+
+
+
+var MyStack = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.queue.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue[this.queue.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.length === 0;
+};
