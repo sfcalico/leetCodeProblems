87ce8d4f1cc95623fdb34e402de6f5580b1c80bc
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-11 10:25:50 -0700

    Create 183.js

---
+++ 183.js
@@ -0,0 +1,10 @@
+// 181. Employees Earning More Than Their Managers (SQL)
+// first submission
+// Runtime: 340 ms, faster than 76.71% of MySQL online submissions for Employees Earning More Than Their Managers.
+// Memory Usage: 0B, less than 100.00% of MySQL online submissions for Employees Earning More Than Their Managers.
+
+// Write your MySQL query statement below
+SELECT a.name AS Employee
+FROM Employee AS a, Employee AS b
+WHERE a.managerId = b.Id
+    AND a.salary > b.salary;
