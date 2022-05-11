bfe7d4f4535b2f1e6993ae51f5ec40bb5edfd494
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-11 09:55:33 -0700

    Update 169.js
    second submission, accepted

--- 169.js
+++ 169.js
@@ -19,3 +19,28 @@ var majorityElement = function(nums) {
     }
     return max;
 };
+
+// second submission
+// Runtime: 64 ms, faster than 95.55% of JavaScript online submissions for Majority Element.
+// Memory Usage: 43.3 MB, less than 90.82% of JavaScript online submissions for Majority Element.
+// 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const hash = {};
+    let max = 0;
+    let maj = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if(!hash[nums[i]]) {
+            hash[nums[i]] = 0;
+        }
+        hash[nums[i]]++;
+        if (hash[nums[i]] > max) {
+            maj = nums[i];
+            max = hash[nums[i]];
+        }
+    }
+    return maj;
+};
