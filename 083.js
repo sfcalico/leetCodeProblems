3d00bde375a6b2d3596cb5156c57d40af4273fe6
Cullen REDDY <87457963+sfcalico@users.noreply.github.com>
2022-05-27 07:57:45 -0700

    Update 083.js

--- 083.js
+++ 083.js
@@ -26,3 +26,19 @@ var deleteDuplicates = function(head) {
     }
     return head;
 };
+
+// Second submission
+// Runtime: 93 ms, faster than 54.40% of JavaScript online submissions for Remove Duplicates from Sorted List.
+// Memory Usage: 43.9 MB, less than 87.36% of JavaScript online submissions for Remove Duplicates from Sorted List.
+
+var deleteDuplicates = function(head) {
+    let current = head;
+    while (current !== null && current.next !== null) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;    
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+};
